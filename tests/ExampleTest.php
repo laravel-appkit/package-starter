20647e5ad773bd000f16dd6b11a3264609860e28
Darren Coutts <couttsdarren@outlook.com>
2020-12-31 17:24:20 +0000

    Improve test directory

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,7 +5,7 @@ namespace AppKit\:package_name_php\Tests;
 class ExampleTest extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    public function trueIsTrue()
     {
         $this->assertTrue(true);
     }
