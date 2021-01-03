391caa19309c053ca4f04726a01626691803004b
Darren Coutts <couttsdarren@outlook.com>
2021-01-03 13:23:24 +0000

    Add article model for testing

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace AppKit\:package_name_php\Tests;
 
+use AppKit\:package_name_php\Tests\Models\Article;
 class ExampleTest extends TestCase
 {
     /** @test */
@@ -9,4 +10,14 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    /** @test */
+    public function artcilesCanBeLoaded()
+    {
+        $articles = factory(Article::class, 5)->make();
+
+        $this->assertEquals(5, Article::count());
+    }
+
+
 }
