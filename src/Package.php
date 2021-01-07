3f2628c6f847ccf0c7e336e2fb65386fe2ca12a9
Darren Coutts <couttsdarren@outlook.com>
2021-01-07 13:56:30 +0000

    Update the skeleton package after building the first package :package:

--- src/Package.php
+++ src/Package.php
@@ -4,5 +4,12 @@ namespace AppKit\:package_name_php;
 
 class :package_name_php
 {
+    private $app;
+
+    public function __construct()
+    {
+        $this->app = app();
+    }
+
     // Build your next great package.
 }
